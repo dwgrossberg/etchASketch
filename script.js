0e7fcc0b4665f43fe90c0685b8616f7733ac4c51
Dan Grossberg <dwgrossberg@gmail.com>
2022-01-31 21:49:01 +0700

    Update box grid to automatically size each box depending on the given room available

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 
 
 function createGrid(number) {
-    const container = document.getElementById('container'); 
+    const container = document.getElementById('container');
+    const boxHeight = String(Math.floor(500/number)) + 'px';
+    const boxWidth = String(Math.floor(500/number)) + 'px';
     for (let i = 0; i < number; i++) { 
         let divColumn = document.createElement('div');
         divColumn.classList.add('gridColumn');
         for (let i = 0; i < number; i++){
             let box = document.createElement('div');
-            box.classList.add('box');
+            box.classList.add('box'); 
+            box.style.height = boxHeight;
+            box.style.width = boxWidth;
             divColumn.appendChild(box);
   } 
         container.appendChild(divColumn);
 }
 }
 
-console.log(createGrid(16));
+console.log(createGrid(10));

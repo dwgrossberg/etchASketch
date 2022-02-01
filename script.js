eada3733378181bcdaefbcc40b24401dd5b15373
Dan Grossberg <dwgrossberg@gmail.com>
2022-02-01 22:02:50 +0700

    Update clear button to actually clear and reset grid

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const container = document.getElementById('container');
+const box = document.querySelectorAll('div.box');
 
 
 function clearGrid(parent) {
@@ -9,7 +11,6 @@ function clearGrid(parent) {
 
 function createGrid(number) {
     if (typeof Number(number) != 'number') return; //end function if no number is entered
-    const container = document.getElementById('container');
     clearGrid(container);
     const boxHeight = String(75/Number(number)) + 'vh';
     const boxWidth = String(90/Number(number)) + 'vw';
@@ -30,18 +31,33 @@ function createGrid(number) {
 //initial grid size upon page load
 createGrid(16);
 
-//add a random rgb color value for each div 'box' upon mouseover
-const box = document.querySelectorAll('div.box');
-box.forEach(box => box.addEventListener('mouseover', function(e){
+
+//mouseover color effect
+function colorSet(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+
+//select a random rbg color for each square
+function randomRGB(e) {
     let r = Math.random, m = Math.round, s = 255;
     let rgb = 'rgb(' + m(r()*s) + ', ' + m(r()*s) + ', ' + m(r()*s) + ')';
     box.style.backgroundColor = rgb;    
 }
-));
 
-//reset sketch box to custom size and remove box coloring
-const button = document.querySelector('button');
-button.addEventListener('click', function(e){
-    createGrid(prompt('Choose a number of squares to create a new sketchpad', ));
-})
+
+//add mouseover functionality to the grid
+box.forEach(item => {
+    item.addEventListener('mouseover', colorSet);
+} 
+);
+
+
+//reset sketch box to original size and remove any box styling
+const buttonClear = document.getElementById('clear');
+buttonClear.addEventListener('click', function(e) {
+    clearGrid(container);
+    createGrid(16);
+}
+);
 

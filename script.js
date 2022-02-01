9fd7bf39f98afc41910ed93d91e96bee3b780a92
Dan Grossberg <dwgrossberg@gmail.com>
2022-02-01 14:55:13 +0700

    Update button function to properly clear grid before creating a new grid

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function createGrid(number) {
     if (typeof Number(number) != 'number') return; //end function if no number is entered
     const container = document.getElementById('container');
     clearGrid(container);
-    const boxHeight = String(80/Number(number)) + 'vh';
-    const boxWidth = String(80/Number(number)) + 'vw';
+    const boxHeight = String(75/Number(number)) + 'vh';
+    const boxWidth = String(90/Number(number)) + 'vw';
     for (let i = 0; i < number; i++) { 
         let divColumn = document.createElement('div');
         divColumn.classList.add('gridColumn');
@@ -30,18 +30,18 @@ function createGrid(number) {
 //initial grid size upon page load
 createGrid(16);
 
-const box = document.querySelectorAll('div.box');
-
 //add a random rgb color value for each div 'box' upon mouseover
+const box = document.querySelectorAll('div.box');
 box.forEach(box => box.addEventListener('mouseover', function(e){
     let r = Math.random, m = Math.round, s = 255;
     let rgb = 'rgb(' + m(r()*s) + ', ' + m(r()*s) + ', ' + m(r()*s) + ')';
-    box.style.backgroundColor = rgb;
+    box.style.backgroundColor = rgb;    
 }
-))
+));
 
 //reset sketch box to custom size and remove box coloring
 const button = document.querySelector('button');
 button.addEventListener('click', function(e){
     createGrid(prompt('Choose a number of squares to create a new sketchpad', ));
 })
+
